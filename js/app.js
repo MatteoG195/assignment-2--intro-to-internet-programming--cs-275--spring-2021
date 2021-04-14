15b6fd045c122803931f90a444fb70e949d82e1c
Matteo Gaudio <matteog1999@gmail.com>
2021-04-14 19:26:42 -0400

    cleared app.js for processing rework, updated eslinter for tab, package.json added support for eslint cli

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+window.onload = () => {
 
-
-window.onLoad()
-{
-  let form = document.querySelector("form");
-  global const cali-recipe = 
-}
+    //TODO: update html to have 2 buttons for the type of rice,
+    //remove form and submission button from structure
+    //make each button load the correct recipe and calculate measurements
+    //from cups of rice textbox
+};
